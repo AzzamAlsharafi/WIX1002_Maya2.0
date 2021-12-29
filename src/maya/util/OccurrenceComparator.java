484e1be3fc89acaa745460ed7c7bd907e3c87262
Azzam Alsharafi <azzamalsharafi@gmail.com>
2021-12-29 18:38:01 +0800

    Edit: sort occurrences so that modules where the student is allowed to register comes first.
    Sort occurrences so that modules where the student is allowed to register comes first.

--- src/maya/util/OccurrenceComparator.java
+++ src/maya/util/OccurrenceComparator.java
@@ -1,6 +1,9 @@
 package maya.util;
 
+import maya.Main;
+import maya.object.Module;
 import maya.object.Occurrence;
+import maya.object.StudentAccount;
 
 import java.util.Comparator;
 
@@ -8,6 +11,17 @@ public class OccurrenceComparator implements Comparator<Occurrence> {
 
     @Override
     public int compare(Occurrence o1, Occurrence o2) {
+        if(Main.currentUser instanceof StudentAccount){
+            boolean isAllowed1 = Main.modules.get(o1.getCode()).checkRestrictions((StudentAccount) Main.currentUser).isBlank();
+            boolean isAllowed2 = Main.modules.get(o2.getCode()).checkRestrictions((StudentAccount) Main.currentUser).isBlank();
+
+            if(isAllowed1 & !isAllowed2){
+                return -1;
+            }else if(isAllowed2 & !isAllowed1){
+                return 1;
+            }
+        }
+
         if (o1.getCode().compareTo(o2.getCode()) == 0) {
             if (o1.getOccurrenceNumber() == o2.getOccurrenceNumber()) {
                 return Integer.compare(o1.getActivityType(), o2.getActivityType());
