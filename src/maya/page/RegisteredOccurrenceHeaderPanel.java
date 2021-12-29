742be9da2fbff7f4179d5bd319bc263641b69db1
Azzam Alsharafi <azzamalsharafi@gmail.com>
2021-12-29 19:09:19 +0800

    Edit: move RegisteredOccurrenceHeaderPanel to its own separate file
    Move RegisteredOccurrenceHeaderPanel to its own separate file.

---
+++ src/maya/page/RegisteredOccurrenceHeaderPanel.java
@@ -0,0 +1,62 @@
+package maya.page;
+
+import maya.util.ColorsManager;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+class RegisteredOccurrenceHeaderPanel extends JPanel {
+    JLabel moduleTitleLabel = new JLabel();
+    JLabel occurrenceNumberLabel = new JLabel();
+    JLabel lastLabel = new JLabel();
+
+    RegisteredOccurrenceHeaderPanel(ModulePanel parent) {
+        if (parent instanceof StudentModulePanel) {
+            lastLabel.setText("Credits");
+            lastLabel.setBorder(new EmptyBorder(0, 0, 0, 15));
+            occurrenceNumberLabel.setBorder(new EmptyBorder(0, 0, 0, 20));
+        } else {
+            lastLabel.setText("Activity");
+            lastLabel.setBorder(new EmptyBorder(0, 0, 0, 10));
+            occurrenceNumberLabel.setBorder(new EmptyBorder(0, 0, 0, 25));
+        }
+
+        moduleTitleLabel.setText("Module");
+        occurrenceNumberLabel.setText("Occ");
+
+        moduleTitleLabel.setBorder(new EmptyBorder(0, 10, 0, 30));
+
+        Color fontColor = ColorsManager.whiteFont;
+        moduleTitleLabel.setForeground(fontColor);
+        occurrenceNumberLabel.setForeground(fontColor);
+        lastLabel.setForeground(fontColor);
+
+        Dimension size = new Dimension(200, 39);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
+
+        Border border1 = BorderFactory.createCompoundBorder(new BevelBorder(BevelBorder.RAISED), new BevelBorder(BevelBorder.LOWERED));
+        Border border = BorderFactory.createCompoundBorder(border1, new BevelBorder(BevelBorder.RAISED));
+        setBorder(border);
+
+        setBackground(ColorsManager.signatureDarkBlue);
+
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+        add(moduleTitleLabel, c);
+
+        c.gridx = 1;
+        add(occurrenceNumberLabel, c);
+
+        c.gridx = 2;
+        add(lastLabel, c);
+    }
+}
